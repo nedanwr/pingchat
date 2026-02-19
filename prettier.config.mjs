192c53a94fb8796cb42d0a2d8481286b9639f90e
Naveed <46922100+nedanwr@users.noreply.github.com>
2026-02-20 02:39:20 +0800

    chore(monorepo): add Prettier configuration file

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none",
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: "always",
+  bracketSpacing: true,
+  bracketSameLine: false
+};
+
+export default config;
