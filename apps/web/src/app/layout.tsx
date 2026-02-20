f4ec57035e5a7c3b40a34a4afe0a651423ef4c3b
Naveed <46922100+nedanwr@users.noreply.github.com>
2026-02-20 18:18:00 +0800

    feat(web): integrate `ThemeProvider` into layout for theme management

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,23 +2,30 @@ import { Geist } from "next/font/google";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 
 import { ConvexClientProvider } from "~/integrations/convex/provider";
+import { ThemeProvider } from "~/integrations/theme/provider";
 import "~/styles/globals.css";
 
 const geist = Geist({
   subsets: ["latin"],
-  variable: "--font-geist-sans",
+  variable: "--font-geist-sans"
 });
 
 export default async function RootLayout({
-  children,
+  children
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>
-        <ConvexAuthNextjsServerProvider>
-          <ConvexClientProvider>{children}</ConvexClientProvider>
-        </ConvexAuthNextjsServerProvider>
-      </body>
-    </html>
+    <ConvexAuthNextjsServerProvider>
+      <ConvexClientProvider>
+        <html
+          lang="en"
+          className={`${geist.variable}`}
+          suppressHydrationWarning
+        >
+          <body>
+            <ThemeProvider>{children}</ThemeProvider>
+          </body>
+        </html>
+      </ConvexClientProvider>
+    </ConvexAuthNextjsServerProvider>
   );
 }
